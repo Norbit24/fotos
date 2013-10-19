9c9826509ac3b7b066a5d323c95db6dd4237917b
Bearded Avenger <nick@pagelines.com>
2013-10-19 09:37:15 -0500

    do 0.9.2

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ require_once( dirname(__FILE__) . '/setup.php' );
 
 class baFotosTheme {
 
-	const version = '0.9.1';
+	const version = '0.9.2';
 
 	function __construct() {
 
--- inc/support-tab.php
+++ inc/support-tab.php
@@ -37,23 +37,17 @@ class fotosSupportTab {
 	        'pos'        		=> 70,
 	        'panel'      		=> array(
 	        	'heading'       => __( 'Fotos Support', 'fotos' ),
-	            'videos'         => array(
-	                'name'      => __( 'Videos', 'fotos' ),
-	               	'type'      => 'call',
-	                'call'      => array($this,'ba_fotos_vids_call'),
-	                'icon'      => 'icon-film'
-	            ),
-	            'docs'      => array(
-	                'name'      => __( 'Help', 'fotos' ),
+	        	'docs'      => array(
+	                'name'      => __( 'Get Help', 'fotos' ),
 	                'icon'      => 'icon-ambulance',
 	                'type'		=> 'call',
-	             	'call'		=> array($this,'ba_fotos_docs_call')
+	             	'call'		=> array($this,'fotos_help_call')
 	            ),
 	            'market'      => array(
 	                'name'      => __( 'Fotos Market', 'fotos' ),
 	                'icon'      => 'icon-shopping-cart',
 	                'type'		=> 'call',
-	             	'call'		=> array($this,'ba_fotos_market_call')
+	             	'call'		=> array($this,'fotos_market_call')
 	            )
 	        )
 	    );
@@ -61,64 +55,62 @@ class fotosSupportTab {
 		return $toolbar;
 	}
 
-	// Video Panel Callback
-	function ba_fotos_vids_call(){
 
+	function fotos_help_call(){
 		?>
+		<div class="fotos-docs-row row">
+			<div class="span3">
+				<p class="zmt" style="margin-bottom:7px;">Stuck and need some help? Check out the videos on your right, or hit the docs link below. If all else fails, hit us up on the forum!</p>
 
-		<div class="row">
-			<div class="span3">
-				<p class="zmt fotos-help"><strong>Getting started with fotos</strong></p>
-				<img src="http://placehold.it/300x184">
-			</div>
-			<div class="span3">
-				<p class="zmt fotos-help"><strong>Working with options</strong></p>
-				<img src="http://placehold.it/300x184">
-			</div>
-			<div class="span3">
-				<p class="zmt fotos-help"><strong>How to setup the contact form</strong></p>
-				<img src="http://placehold.it/300x184">
-			</div>
-			<div class="span3">
-				<p class="zmt fotos-help"><strong>How to create a gallery</strong></p>
-				<img src="http://placehold.it/300x184">
-			</div>
-		</div>
-		<div class="row">
-			<div class="span3">
-				<p class="zmt fotos-help"><strong>How to install skins</strong></p>
-				<img src="http://placehold.it/300x184">
-			</div>
-			<div class="span3">
-				<p class="zmt fotos-help"><strong>Installing Fotos plugins</strong></p>
-				<img src="http://placehold.it/300x184">
-			</div>
-			<div class="span3">
+				<a class="btn btn-info"><i class="icon-user"></i> &nbsp;Support Forum</a>
+				<a class="btn btn-info"><i class="icon-folder-close"></i> &nbsp;Docs</a>
+
+				<p style="margin-bottom:7px;">We're also on those social places!</p>
+				<a class="btn btn-info"><i class="icon-twitter-sign"></i> &nbsp;Follow Us</a>
+				<a class="btn btn-info"><i class="icon-facebook-sign"></i> &nbsp;Be a Fan</a>
 
 			</div>
-			<div class="span3">
+			<div class="span9">
+				<div class="row">
+					<div class="span3">
+						<p class="zmt fotos-help"><strong>Getting started with fotos</strong></p>
+						<img src="http://placehold.it/300x184">
+					</div>
+					<div class="span3">
+						<p class="zmt fotos-help"><strong>Working with options</strong></p>
+						<img src="http://placehold.it/300x184">
+					</div>
+					<div class="span3">
+						<p class="zmt fotos-help"><strong>How to setup the contact form</strong></p>
+						<img src="http://placehold.it/300x184">
+					</div>
+					<div class="span3">
+						<p class="zmt fotos-help"><strong>How to create a gallery</strong></p>
+						<img src="http://placehold.it/300x184">
+					</div>
+				</div>
+				<div class="row">
+					<div class="span3">
+						<p class="zmt fotos-help"><strong>How to install skins</strong></p>
+						<img src="http://placehold.it/300x184">
+					</div>
+					<div class="span3">
+						<p class="zmt fotos-help"><strong>Installing Fotos plugins</strong></p>
+						<img src="http://placehold.it/300x184">
+					</div>
+					<div class="span3">
 
+					</div>
+					<div class="span3">
+
+					</div>
+				</div>
 			</div>
-		</div>
-		<?php
-
+		</div><?php
 	}
 
-	// Docs Panel Callback
-	function ba_fotos_docs_call(){
-		?>
 
-		<p class="zmt">Are you stuck and need some help? Have no fear, the Fotos Heros are here! You can also follow us on Twitter to get up to the minute info on news and new updates.</p>
-		<div class="btn-group">
-			<a class="btn btn-info"><i class="icon-user"></i> &nbsp;Support Forum</a>
-			<a class="btn btn-info"><i class="icon-folder-close"></i> &nbsp;Docs</a>
-			<a class="btn btn-info"><i class="icon-twitter-sign"></i> &nbsp;Follow Us</a>
-			<a class="btn btn-info"><i class="icon-facebook-sign"></i> &nbsp;Be a Fan</a>
-		</div>
-		<?php
-	}
-
-	function ba_fotos_market_call(){
+	function fotos_market_call(){
 		?>
 		<p class="span12">The <a href="http://nickhaskins.co/fotos" target="_blank">Fotos Market</a> is where you can find cool plugins and amazing skins to compliment your site with. We're going to display those products here soon, but in the mean time head on over and <a href="http://nickhaskins.co/fotos" target="_blank">check out</a> the market!</p>
 		<p>P.S. - Want to be a Fotos Author and earn a nice little recurring income? <a href="http://nickhaskins.co/fotos-authors/" target="_blank">Apply here</a>.</p>
