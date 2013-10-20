dbad354f37375871b6149bd43df293f07d3fc69f
Bearded Avenger <nick@pagelines.com>
2013-10-20 17:41:34 -0500

    do captions on boxes

--- sections/fotos-boxes/section.php
+++ sections/fotos-boxes/section.php
@@ -11,6 +11,12 @@ class baFotosBoxes extends PageLinesSection{
 
 	const version = '1.0';
 
+	function section_head(){
+		// Widget Titles
+		if($this->opt('ba_fotos_box_cap_font'))
+			echo load_custom_font( $this->opt('ba_fotos_box_cap_font'),'.ba-fotos-box-cap');
+	}
+
 	function section_template(){
 
 		$output 	= '';
@@ -18,7 +24,12 @@ class baFotosBoxes extends PageLinesSection{
 		$box_array 	= $this->opt('fotos_box_array');
 		$layout 	= $this->opt('fotos_box_col') ? $this->opt('fotos_box_col') : '3col';
 
-		if( is_array($box_array) ){
+		$captxtcolor = pl_hashify($this->opt('ba_fotos_box_cap_text'));
+		$capbgcolor = pl_hashify($this->opt('ba_fotos_box_cap_back'));
+
+		$capstyle 	= ($captxtcolor || $capbgcolor) ? sprintf('style="background:%s;color:%s;"',$capbgcolor,$captxtcolor) : false;
+
+		if ( is_array($box_array) ){
 
 			$boxes = count($box_array);
 
@@ -27,8 +38,22 @@ class baFotosBoxes extends PageLinesSection{
 		  		$last 		= ($count == $layout) ? 'last' : false;
 				$getimg 	= pl_array_get('img', $box, 'http://placehold.it/50/50');
 				$link		= pl_array_get('link',$box);
+				$cap 		= pl_array_get('caption', $box);
 
-				$image 		= sprintf('<a href="%s"><img class="ba-fotos-box-img" alt="" src="%s" /></a>',$link,$getimg);
+				if ($link) {
+
+					if ($cap){
+						$image 		= sprintf('<a class="ba-fotos-box-link-wrap" href="%s"><img class="ba-fotos-box-img" alt="" src="%s" /><span class="ba-fotos-box-cap" %s>%s</span></a>',$link,$getimg,$capstyle,$cap);
+					} else {
+						$image 		= sprintf('<a class="ba-fotos-box-link-wrap" href="%s"><img class="ba-fotos-box-img" alt="" src="%s" /></a>',$link,$getimg);
+					}
+
+				} else {
+
+					$image 		= sprintf('<img class="ba-fotos-box-img" alt="" src="%s" />',$getimg);
+
+				}
+
 				$output    .= sprintf('<div class="ba-fotos-box-item %s">%s</div>',$last,$image);
 
 				$count++;
@@ -75,8 +100,41 @@ class baFotosBoxes extends PageLinesSection{
 					'label'		=> __( 'Link', 'fotos' ),
 					'type'		=> 'text'
 				),
-			),
+				array(
+					'key'		=> 'caption',
+					'label'		=> __( 'Link Caption (optional if using link)', 'fotos' ),
+					'type'		=> 'text'
+				)
+			)
 
+		);
+
+		$options[] = array(
+			'key'				=> 'ba_fotos_box_design',
+			'type'				=> 'multi',
+			'title'				=> __('Box Captions Design','fotos'),
+			'opts'				=> array(
+				array(
+					'key'		=> 'ba_fotos_box_cap_back',
+					'type'		=> 'color',
+					'default'	=> '#0077CC',
+					'title'		=> __('Box Caption Background','fotos')
+				),
+				array(
+					'key'		=> 'ba_fotos_box_cap_text',
+					'type'		=> 'color',
+					'default'	=> '#FFF',
+					'title'		=> __('Box Caption Text','fotos')
+				),
+				array(
+					'title'   	=> __('Box Caption Font', 'fotos'),
+				    'type'    	=> 'fonts',
+				    'default'	=> 'open_sans',
+				    'key'		=> 'ba_fotos_box_cap_font',
+					'help' 		=> __('This controls the box caption font for this section.' , 'fotos'),
+				)
+			),
+			'help'				=> __('This','fotos')
 		);
 
 
