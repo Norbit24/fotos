faf2825831d5ddba6af93192463b331ffaaba6a8
Bearded Avenger <email@nickhaskins.com>
2013-11-11 12:36:37 -0600

    few support videos and lnkks

--- inc/support-tab.php
+++ inc/support-tab.php
@@ -62,47 +62,41 @@ class fotosSupportTab {
 			<div class="span3">
 				<p class="zmt" style="margin-bottom:7px;"><?php _e('Stuck and need some help? Check out the videos on your right, or hit the docs link below. If all else fails, hit us up on the forum!','fotos');?></p>
 
-				<a class="btn btn-info"><i class="icon-user"></i> &nbsp;<?php _e('Support Forum','fotos');?></a>
-				<a class="btn btn-info"><i class="icon-folder-close"></i> &nbsp;<?php _e('Docs','fotos');?></a>
+				<a class="btn btn-info" href="http://nickhaskins.co/forum/fotos-support/" target="_blank"><i class="icon-user"></i> &nbsp;<?php _e('Support Forum','fotos');?></a>
+				<a class="btn btn-info" href="http://docs.fotostheme.com" target="_blank"><i class="icon-folder-close"></i> &nbsp;<?php _e('Docs','fotos');?></a>
 
 				<p style="margin-bottom:7px;"><?php _e('We\'re also on those social places!','fotos');?></p>
-				<a class="btn btn-info"><i class="icon-twitter-sign"></i> &nbsp;<?php _e('Follow Us','fotos');?></a>
-				<a class="btn btn-info"><i class="icon-facebook-sign"></i> &nbsp;<?php _e('Be a Fan','fotos');?></a>
+				<a href="http://twitter.com/fotostheme" target="_blank" class="btn btn-info"><i class="icon-twitter-sign"></i> &nbsp;<?php _e('Follow Us','fotos');?></a>
+				<a href="http://facebook.com/fotostheme" target="_blank" class="btn btn-info"><i class="icon-facebook-sign"></i> &nbsp;<?php _e('Be a Fan','fotos');?></a>
 
 			</div>
 			<div class="span9">
 				<div class="row">
-					<div class="span3">
+					<div class="span4">
 						<p class="zmt fotos-help"><strong><?php _e('Getting started with fotos','fotos');?></strong></p>
-						<img src="http://placehold.it/300x184">
+						<iframe src="//player.vimeo.com/video/79112855?title=0&amp;byline=0&amp;portrait=0" width="300" height="169" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 					</div>
-					<div class="span3">
+					<div class="span4">
 						<p class="zmt fotos-help"><strong><?php _e('Working with options','fotos');?></strong></p>
-						<img src="http://placehold.it/300x184">
+						<iframe src="//player.vimeo.com/video/71111070?title=0&amp;byline=0&amp;portrait=0" width="300" height="169" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 					</div>
-					<div class="span3">
+					<div class="span4">
 						<p class="zmt fotos-help"><strong><?php _e('How to setup the contact form','fotos');?></strong></p>
-						<img src="http://placehold.it/300x184">
-					</div>
-					<div class="span3">
-						<p class="zmt fotos-help"><strong><?php _e('How to create a gallery','fotos');?></strong></p>
-						<img src="http://placehold.it/300x184">
+						<iframe src="//player.vimeo.com/video/79117858?title=0&amp;byline=0&amp;portrait=0" width="300" height="169" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 					</div>
 				</div>
 				<div class="row">
-					<div class="span3">
+					<div class="span4">
+						<p class="zmt fotos-help"><strong><?php _e('How to create a gallery','fotos');?></strong></p>
+						<iframe src="//player.vimeo.com/video/71111070?title=0&amp;byline=0&amp;portrait=0" width="300" height="169" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+					</div>
+					<div class="span4">
 						<p class="zmt fotos-help"><strong><?php _e('How to install skins','fotos');?></strong></p>
 						<img src="http://placehold.it/300x184">
 					</div>
-					<div class="span3">
+					<div class="span4">
 						<p class="zmt fotos-help"><strong><?php _e('Installing Fotos plugins','fotos');?></strong></p>
 						<img src="http://placehold.it/300x184">
-					</div>
-					<div class="span3">
-
-					</div>
-					<div class="span3">
-
 					</div>
 				</div>
 			</div>
